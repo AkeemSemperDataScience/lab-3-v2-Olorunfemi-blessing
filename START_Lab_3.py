ba9266c19f4122ff472978fa20c2a314ee33d162
blessing-olorunfemi <bolorunfemi1@nait.ca>
2024-10-05 10:34:46 -0600

    commit2

--- START_Lab_3.py
+++ START_Lab_3.py
@@ -18,7 +18,17 @@ def lab3Question2(decimal_number):
     # Take in an argument of a float (decimal) number.
     # Return "zero" if the number is 0, "positive" if the number is positive, and "negative" if the number is negative
     # Return "invalid" if the input is not a float
-    pass
+        
+    if type(decimal_number) == float: 
+            if decimal_number == 0:
+                print('Zero')
+            elif decimal_number > 0:
+                print('Positive')
+            elif decimal_number != float:
+                print('Invalid')
+            else:
+                print('Negative')
+    return decimal_number
 
 def lab3Question3(year):
     # Take in a number that represents a year
